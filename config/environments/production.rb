e9040c6e59617d76c67ba5a07af54ef0ea51b528
Alicia Bajwa <alicia.bajwa@gmail.com>
2015-03-22 19:40:35 -0400

    Devise Setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Use actual host name here
+  config.action_mailer.default_url_options = { :host => 'babyjavu.herokuapp.com' }
 end
